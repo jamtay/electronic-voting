4df14e4f740df2315154a257e952cc46fc8ec2d7
James Taylor <sc13jt@leeds.ac.uk>
2017-03-13 12:55:23 +0000

    Added background and style to reg and vote page

--- app/Global.java
+++ app/Global.java
@@ -9,6 +9,10 @@ public class Global extends GlobalSettings {
    */
   public void onStart(Application app) {
     UserDatabase.addUserInfo("John Smith", "smith@example.com", "password", "admin");
-    UserDatabase.addUserInfo("James Taylor", "james@james.com", "james", "voter");
+    UserDatabase.addUserInfo("James Taylor", "james@james.com", "password", "voter");
+    UserDatabase.addUserInfo("James Taylor2", "james@james.com2", "password", "voter");
+    UserDatabase.addUserInfo("James Taylor3", "james@james.com3", "password", "voter");
+    UserDatabase.addUserInfo("James Taylor4", "james@james.com4", "password", "voter");
+    UserDatabase.addUserInfo("James Taylor5", "james@james.com", "password", "voter");
   }
 }
